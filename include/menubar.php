b941637a8850ef1e10102934a06754bfd623bc17
David Thong Nguyen <thnguyen2808@gmail.com>
2015-12-07 15:59:47 -0500

    Using PHP includes

---
+++ include/menubar.php
@@ -0,0 +1,14 @@
+<div class="menubar">
+
+    <ul>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Gallery</li>
+        <li>Shop</li>
+        <li><a href="contact.php">Contact</a></li>
+        <li>Events</li>
+        <hr>
+        <img id="logo" src="media/logo2.png">
+    </ul>
+
+</div>
--- index.php
+++ index.php
@@ -8,22 +8,8 @@
 </head>
 <body>
 
-<div class="menubar">
-
-    <ul>
-        <li>About</li>
-        <li>Menu</li>
-        <li>Gallery</li>
-        <li>Shop</li>
-        <li><a href="contact.php">Contact</a></li>
-        <li>Events</li>
-        <hr>
-        <img id="logo" src="media/logo2.png">
-    </ul>
-
-</div>
-
-
+    <?php include("include/menubar.php"); ?>
+    
 
 </body>
 </html>
