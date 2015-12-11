b2c0107e5b3462c2a13ac4caefecd59d059589c2
colejohnson <cjinak810@gmail.com>
2015-12-11 15:46:03 -0500

    Terrific.

--- contact.php
+++ contact.php
@@ -47,15 +47,15 @@
 
         <form method="post" action="include/send-contact.php">
             <div class="form-group">
-                <input name="name" class="form-control" type="text" placeholder="Enter your Name">
+                Name: <input name="name" class="form-control" type="text" placeholder="Enter your Name">
             </div>
             <br>
             <div class="form-group">
-                <input name="email" class="form-control" type="email" placeholder="Enter your Email">
+                Email:<input name="email" class="form-control" type="email" placeholder="Enter your Email">
             </div>
             <br>
             <div class="form-group">
-                <textarea name="message" class="form-control" rows="10" placeholder="Enter your Message"></textarea>
+                Message:<textarea name="message" class="form-control" rows="10" placeholder="Enter your Message"></textarea>
             </div>
             <div class="form-group">
                 <input class="btn btn-success btn-block" type="submit">
