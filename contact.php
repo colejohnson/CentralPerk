941f86c5df1992a411fe38ae679d2e352d04d79d
colejohnson <cjinak810@gmail.com>
2015-12-11 15:34:35 -0500

    Great.

--- contact.php
+++ contact.php
@@ -4,19 +4,43 @@
     <meta charset="UTF-8">
     <title>Contact Us | Central Perk Cafe</title>
     <?php include("include/head.php"); ?>
+    <script src="http://maps.googleapis.com/maps/api/js"></script>
+
+    <script>
+          function initialize() {
+
+
+            var mapProp = {
+                center: new google.maps.LatLng(40.748775, -73.989785),
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+          }
+
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+        </script>
 </head>
 <body>
     
     <?php include("include/header.php"); ?>
     
     <div class="maincontainer">
+
+        <h1 style="font-size:2em;">Drop Us a Line!<h1>
+        <hr>
+
         <form method="post" action="include/send-contact.php">
             <div class="form-group">
                 <input name="name" class="form-control" type="text" placeholder="Enter your Name">
             </div>
+            <br>
             <div class="form-group">
                 <input name="email" class="form-control" type="email" placeholder="Enter your Email">
             </div>
+            <br>
             <div class="form-group">
                 <textarea name="message" class="form-control" rows="10" placeholder="Enter your Message"></textarea>
             </div>
@@ -24,6 +48,23 @@
                 <input class="btn btn-success btn-block" type="submit">
             </div>
         </form>
+
+
+        <div id="mapwrap">
+            <div id="googleMap" style="width:300px;height:300px;"></div>
+
+                     <br>
+                             <p>Central Perk Cafe</p>
+                             <p>116 W 32nd St, New York, NY 10001</p>
+                             <p>(212) 695-9550</p>
+
+
+        </div>
+
+
+
+
+
     </div>
     
     <?php include("include/footer.php"); ?>
