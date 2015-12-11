984d0803d7eb93cb73150da86fc5711833f23d78
anuj530 <anuj305@gmail.com>
2015-12-11 15:34:02 -0500

    events.php

---
+++ events.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Central Perk</title>
+    <?php include("include/head.php"); ?>
+</head>
+
+<body>
+
+    <?php include("include/header.php"); ?>
+
+    <div class = "maincontainer">
+
+        <div class = "textcontainer">
+
+            <h4> This Week</h4>
+
+                
+              
+                <img src="gallery/gallery3.png" alt="about" align="right" style="width:40%; z-index:2; position: relative; padding:2em;">
+
+                <h3>Phoebe Buffay LIVE</h3>
+                <h4>w/ Special Guest Ross Geller</h4>
+                <h4> 6-8PM </h4>
+                <p> Phoebe Buffay is an american cafe artist mostly famous for her global hit 'Smelly cat'!! Don't miss her this Friday 
+                  only at Central Perk.</p>
+                <p> Ross Geller is a fabulos keyboard player famous for his cat and cow noises, joining Phoebe Buffay this Friday only at
+                  Central Perk. </p>
+                  
+          
+                
+
+
+
+                  <img src="gallery/gallery5.png" alt="about" align="left" style="width:40%; z-index:500000; position: relative; padding:2em;">
+
+           
+
+                        <a href="menu.php";><h2 style="font-family: 'DadsRecipeRegular', sans-serif; left:20%; font-size:2.5em;"> See our menu!</h2><a/>
+
+    </div>
+    
+    <?php include("include/footer.php"); ?>
+    
+    
+</body>
+
+</html>
+
+
